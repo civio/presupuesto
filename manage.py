66d9bb3a0bd24084c1e6ce4c64da5052113a5e94
Salvador Pérez <sisekeom@gmail.com>
2016-02-02 12:08:45 +0100

    Add a first version of the watcher's callback.

--- manage.py
+++ manage.py
@@ -6,5 +6,23 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
     from django.core.management import execute_from_command_line
+    from dj_static import Cling, MediaCling
+    from local_settings import ENV
 
-    execute_from_command_line(sys.argv)
+    if sys.argv[1] == 'livereload':
+
+        import formic
+
+        from django.core.wsgi import get_wsgi_application
+        from livereload import Server
+
+        application = get_wsgi_application()
+        server = Server(Cling(application))
+
+        # Add your watch
+        for filepath in formic.FileSet(include="*/**.less"):
+            server.watch(filepath, './reload_less.sh %s' % ENV['THEME'])
+
+        server.serve(port=8000)
+    else:
+        execute_from_command_line(sys.argv)
---
+++ reload_less.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+lessc ./$1/static/stylesheets/main.less ./$1/static/stylesheets/main.css
