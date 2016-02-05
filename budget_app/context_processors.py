67416560221505e774c6bf5b6265538970bed0a6
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2016-02-05 13:00:52 +0100

    Add support to variables COOKIES_URL, SHOW_PAYMENTS, SHOW_TAX_RECEIPT & SHOW_COUNTIES_AND_TOWNS in theme settings
    We expect in theme/settings.py this variables:
    
    # Show Payments section in menu & home options. Default: False.
    SHOW_PAYMENTS = True
    
    # Show Tax Receipt section in menu & home options. Default: False.
    SHOW_TAX_RECEIPT = True
    
    # Show Counties & Towns links in Policies section in menu & home
    options. Default: False.
    SHOW_COUNTIES_AND_TOWNS = False
    
    # External URL for Cookies Policy (if empty we use out template
    page/cookies.html)
    COOKIES_URL = 'http://miayuntamiento.es/cookies/'

--- budget_app/context_processors.py
+++ budget_app/context_processors.py
@@ -2,3 +2,13 @@ from local_settings import ENV
 
 def analytics_processor(request):
     return {'analytics_code': ENV.get('GA_CODE', 'UA-36118896-1')}
+
+def cookies_url_processor(request):
+    return { 'cookies_url': False if not settings.COOKIES_URL else settings.COOKIES_URL }
+
+def show_options_processor(request):
+    return {
+      'show_payments':            settings.SHOW_PAYMENTS,
+      'show_tax_receipt':         settings.SHOW_TAX_RECEIPT,
+      'show_counties_and_towns':  settings.SHOW_COUNTIES_AND_TOWNS
+    }
