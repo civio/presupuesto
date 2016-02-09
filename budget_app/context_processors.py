c712371dcec883e1e23e577fff71a44ab2ba2224
David Cabo <david.cabo@gmail.com>
2016-02-09 02:40:09 +0100

    Add settings dependency and avoid crashing if setting is not found

--- budget_app/context_processors.py
+++ budget_app/context_processors.py
@@ -1,14 +1,16 @@
 from local_settings import ENV
+from django.conf import settings
 
 def analytics_processor(request):
-    return {'analytics_code': ENV.get('GA_CODE', 'UA-36118896-1')}
+    return { 'analytics_code': ENV.get('GA_CODE', 'UA-36118896-1') }
 
 def cookies_url_processor(request):
-    return { 'cookies_url': False if not settings.COOKIES_URL else settings.COOKIES_URL }
+    return { 'cookies_url': False if not hasattr(settings, 'COOKIES_URL') else settings.COOKIES_URL }
 
 def show_options_processor(request):
     return {
-      'show_payments':            settings.SHOW_PAYMENTS,
-      'show_tax_receipt':         settings.SHOW_TAX_RECEIPT,
-      'show_counties_and_towns':  settings.SHOW_COUNTIES_AND_TOWNS
+      # False by default if setting is not found
+      'show_payments':            hasattr(settings, 'SHOW_PAYMENTS') and settings.SHOW_PAYMENTS,
+      'show_tax_receipt':         hasattr(settings, 'SHOW_TAX_RECEIPT') and settings.SHOW_TAX_RECEIPT,
+      'show_counties_and_towns':  hasattr(settings, 'SHOW_COUNTIES_AND_TOWNS') and settings.SHOW_COUNTIES_AND_TOWNS
     }
