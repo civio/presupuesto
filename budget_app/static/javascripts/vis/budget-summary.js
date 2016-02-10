4ac810fbc83cac1e6fdde1cfc76b1978bf22a7a6
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2016-02-10 21:04:15 +0100

    Split setup & update in BudgetSummary
    Now we can use one instance of BudgetSummary & update when year or tab
    changes.
    We also add a transition in bars width when year changes.

--- budget_app/static/javascripts/vis/budget-summary.js
+++ budget_app/static/javascripts/vis/budget-summary.js
@@ -1,52 +1,121 @@
-function BudgetSummary(selector, breakdown, areaNames, colorScale, field, year) {
+function BudgetSummary(selector) {
 
-  // Group breakdown by area
-  var areaAmounts = {};
-  var totalAmount = 0;
-  var i, area;
+  var year          = null,
+      view          = null,
+      areaNames     = null,
+      areaAmounts   = {},
+      totalAmount   = 0,
+      existingAreas = [],
+      $bar          = $('<div class="budget-summary"></div>'),
+      $barItems     = null;
+  
 
-  for (i in breakdown.sub) {
-    var policyAmount = breakdown.sub[i][field][year];
-    if ( policyAmount !== undefined ) {
-      area = i[0];
-      areaAmounts[area] = (areaAmounts[area]||0) + policyAmount;
-      totalAmount = totalAmount + policyAmount;
-    }
-  }
-
-  // Sort areas
-  var existingAreas = [];
-  for (area in areaAmounts) existingAreas.push(area);
-  existingAreas.sort(function(a, b) { return areaAmounts[b] - areaAmounts[a]; });
-
-  // Render
-  var $bar = $('<div class="budget-summary"></div>');
+  // Insert bar in the DOM
   $(selector).empty().append( $bar );
 
-  for (i = 0; i < existingAreas.length; i++) {
-    area = existingAreas[i];
-    var percentage = 100 * areaAmounts[area] / totalAmount;
-    // Hide  Labels if area is small (< 6% width)
-    var label = (percentage >= 6 ) ? areaNames[area] : '';
-    var amountLabel = ( percentage >= 6 ) ? formatDecimal(percentage, 1)+'<small>%</small>' : '';
-    
-    $bar.append( '<div class="budget-summary-item" style="width:'+percentage+'%;">'+
-                  '<div class="budget-summary-bar" data-id="'+area+'" style="background-color: '+colorScale[Number(area)]+';">'+amountLabel+
-                  '</div><div class="budget-summary-label">'+label+'</div></div>');
-  }
 
-  var $barItems = $bar.find('.budget-summary-item');
-  
-  // Hover styles
-  $barItems.find('.budget-summary-bar')
-    .mouseover(function(e){
-      $barItems.addClass('inactive');
-      $(this).parent().removeClass('inactive').addClass('active');
-      // Hover Treemap Chart related items
-      $( selector.replace('Summary', 'Chart') ).find('.cell:not(.cell-'+$(this).data('id')+')').addClass('out');
-    })
-    .mouseout(function(e){
-      $barItems.removeClass('inactive active');
-      $( selector.replace('Summary', 'Chart') ).find('.cell.out').removeClass('out');
-    });
+  // Setup Data
+  this.setupData = function( _breakdown, _areaNames, _colorScale, _field, _year ){
+
+    areaNames   = _areaNames;
+    colorScale  = _colorScale;
+
+    // Group breakdown by area
+    var i, area;
+    for (i in _breakdown.sub) {
+      var policyAmount = _breakdown.sub[i][_field][_year];
+      if ( policyAmount !== undefined ) {
+        area = i[0];
+        areaAmounts[area] = (areaAmounts[area]||0) + policyAmount;
+        totalAmount = totalAmount + policyAmount;
+      }
+    }
+
+    // Sort areas
+    existingAreas = [];
+    for (area in areaAmounts) existingAreas.push(area);
+    existingAreas.sort(function(a, b) { return areaAmounts[b] - areaAmounts[a]; });
+  };
+
+
+  // Setup Items
+  this.setupItems = function(){
+
+    // Clear conteiner
+    $bar.empty();
+
+    // Render Items
+    var i,
+        area,
+        percentage,
+        label,
+        amountLabel;
+
+    for (i = 0; i < existingAreas.length; i++) {
+      area = existingAreas[i];
+      percentage = 100 * areaAmounts[area] / totalAmount;
+      // Hide  Labels if area is small (< 6% width)
+      label = (percentage >= 6 ) ? areaNames[area] : '';
+      amountLabel = ( percentage >= 6 ) ? formatDecimal(percentage, 1)+'<small>%</small>' : '';
+      
+      $bar.append( '<div class="budget-summary-item" style="width:'+percentage+'%;">'+
+                    '<div class="budget-summary-bar" data-id="'+area+'" style="background-color: '+colorScale[Number(area)]+';">'+amountLabel+
+                    '</div><div class="budget-summary-label">'+label+'</div></div>');
+    }
+
+    $barItems = $bar.find('.budget-summary-item');
+    
+    // Hover events
+    $barItems.find('.budget-summary-bar')
+      .mouseover(function(e){
+        $barItems.addClass('inactive');
+        $(this).parent().removeClass('inactive').addClass('active');
+        // Hover Treemap Chart related items
+        $('#'+view+'ChartContainer .cell:not(.cell-'+$(this).data('id')+')').addClass('out');
+      })
+      .mouseout(function(e){
+        $barItems.removeClass('inactive active');
+        $('#'+view+'ChartContainer .cell.out').removeClass('out');
+      });
+  };
+
+
+  // Update Items
+  this.updateItems = function(){
+
+    var i,
+        area,
+        percentage,
+        amountLabel;
+
+    for (i = 0; i < existingAreas.length; i++) {
+      area = existingAreas[i];
+      percentage = 100 * areaAmounts[area] / totalAmount;
+      // Hide  Labels if area is small (< 6% width)
+      amountLabel = ( percentage >= 6 ) ? formatDecimal(percentage, 1)+'<small>%</small>' : '';
+
+      $($barItems.get(i)).css('width', percentage+'%').find('.budget-summary-bar').html(amountLabel);
+    }
+  };
+
+
+  // Update
+  this.update = function( _breakdown, _areaNames, _colorScale, _field, _view, _year ){
+
+    if (view == _view && year == _year) return;
+
+    // Setup
+    if (view != _view) {
+      this.setupData( _breakdown, _areaNames, _colorScale, _field, _year );
+      this.setupItems();
+    }
+    // Update
+    else {
+      this.setupData( _breakdown, _areaNames, _colorScale, _field, _year );
+      this.updateItems();
+    }
+
+    year = _year;
+    view = _view;
+  };
 }
