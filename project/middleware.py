d0a50a34db3e3c5eb581ba81c3556409f1510646
David Cabo <david.cabo@gmail.com>
2016-02-19 03:00:22 +0100

    Try to improve cache efficiency by ignoring Accept-Language. See civio/presupuesto-management#74

--- project/middleware.py
+++ project/middleware.py
@@ -2,12 +2,21 @@ from django.middleware.cache import UpdateCacheMiddleware
 
 import re
 
-# We need to remove Google Analytics cookies or they'll break the cache
-# as soon as there's a "Vary: Cookie" header, which is our case.
-# See https://github.com/aragonopendata/presupuesto/issues/12 for more info.
 class SmartUpdateCacheMiddleware(UpdateCacheMiddleware):
     STRIP_RE = re.compile(r'\b(_[^=]+=.+?(?:; |$))')
 
     def process_request(self, request):
+        # We need to remove Google Analytics cookies or they'll break the cache
+        # as soon as there's a "Vary: Cookie" header, which is our case.
+        # See https://github.com/aragonopendata/presupuesto/issues/12 for more info.
         cookie = self.STRIP_RE.sub('', request.META.get('HTTP_COOKIE', ''))
         request.META['HTTP_COOKIE'] = cookie
+
+        # And we're going a step further here: since we don't do language negotiation,
+        # (i.e. we never try to guess what's the user preferred language, we always
+        # default to the same one on the homepage, and every other URL is prefixed
+        # with the locale, so there's no ambiguity) we can remove the Accept-Language
+        # request header.
+        # Copied from https://djangosnippets.org/snippets/218/ (although different goal)
+        if request.META.has_key('HTTP_ACCEPT_LANGUAGE'):
+            del request.META['HTTP_ACCEPT_LANGUAGE']
