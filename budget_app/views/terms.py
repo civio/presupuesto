6a7d8b507075444b746ca513ed4f2a0656c419a8
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2016-02-26 17:36:22 +0100

    Add new Pagination implementation in terms view

--- budget_app/views/terms.py
+++ budget_app/views/terms.py
@@ -1,18 +1,19 @@
 from coffin.shortcuts import render_to_response
-from django.core.paginator import Paginator
+from paginator import DiggPaginator as Paginator
 from django.utils.translation import ugettext as _
 from budget_app.models import GlossaryTerm
 from helpers import *
 
+PAGE_LENGTH = 10
 
 def terms(request):
     c = get_context(request, css_class='body-glossary', title=_('Inicio'))
 
     c['query'] = request.GET.get('q', '')
     c['query_string'] = "q=%s&" % (c['query'])
-    page = request.GET.get('page', 1)
+    c['page'] = request.GET.get('page', 1)
 
-    results = Paginator(list(GlossaryTerm.objects.search(c['query'], request.LANGUAGE_CODE)), 10)
-    c['terms'] = results.page(page)
+    results = Paginator(list(GlossaryTerm.objects.search(c['query'], request.LANGUAGE_CODE)), PAGE_LENGTH, body=6, padding=2)
+    c['terms'] = results.page(c['page'])
 
     return render_to_response('terms/index.html', c)
