d905fb0c5d52d4abf5089892880a1871fb74ed0c
David Cabo <david.cabo@gmail.com>
2016-02-29 01:46:05 +0100

    Basic DataTables implementation for tax receipt. civio/presupuesto-management#42

--- budget_app/static/javascripts/grid_helpers.js
+++ budget_app/static/javascripts/grid_helpers.js
@@ -3,22 +3,25 @@
  */
 
 // TODO: This file needs some love to make it more Object Oriented. 
+
+// FIXME: I should get rid of this when we remove SlickGrid
 // We could then get rid of this awful global variables :/ We use them to remember the sorting
 // status on UI updates (i.e. when changing the year), since we rebuild the grid on each page update.
 // (I tried using getSortColumns() from the grid directly, to avoid tracking state myself, but
 // couldn't get it to work)
 var currentSortColumn = -1;
-var currentSortOrder = false;
+var currentSortOrder = null;
 
+// FIXME: I should get rid of this when we remove SlickGrid
 // Poor-man's auto key generator
 var id = 0;
 
 // Display column name depending on multi-level status
-function rowNameFormatter(row, cell, value, columnDef, dataContext) {
-  var spacer = "<span style='display:inline-block;height:1px;width:" + (25 * dataContext["indent"]) + "px'></span>";
+function rowNameFormatter(value, type, item) {
+  var spacer = "<span style='display:inline-block;height:1px;width:" + (25 * item["indent"]) + "px'></span>";
   var valuewrap = "<span class='slick-cell-text toggle'>" + value + "</span>";
-  if (dataContext.sub && !$.isEmptyObject(dataContext.sub)) {
-    if (dataContext._expanded) {
+  if (item.sub && !$.isEmptyObject(item.sub)) {
+    if (item._expanded) {
       return spacer + " <span class='toggle collapse'></span>" + valuewrap;
     } else {
       return spacer + " <span class='toggle expand'></span>" + valuewrap;
@@ -35,11 +38,13 @@ function rowNameFormatter(row, cell, value, columnDef, dataContext) {
 // if something is not present we know it's because the assigned amount is zero. 
 // An item may show up on one side (budget) but not the other (execution), hence
 // displaying a zero (and not just a blank) is the right thing to do.
-function getBreakdownValue(item, columnDef) {
-  if ( item.root != null && item.root.years[columnDef.column] === undefined )
-    return null;
+function getBreakdownValueFunction(field, column) {
+  return function(item) {
+    if ( item.root != null && item.root.years[column] === undefined )
+      return null;
 
-  return item[columnDef.field][columnDef.column] || 0;
+    return item[field][column] || 0;
+  }
 }
 
 // Convert a BudgetBreakdown object to an Array understood by SlickGrid
@@ -71,17 +76,120 @@ function breakdownToTable(breakdown, indent) {
   return gridData;
 }
 
-// Make the SlickGrid data access mechanism more flexible
-function columnValueExtractor(item, columnDef) {
-  // Allow overriding at a column level
-  if (columnDef.getter)
-    return columnDef.getter(item, columnDef);
+// Make the data access mechanism more flexible
+function columnValueExtractor(item, getter) {
+  if (typeof getter === 'function')
+    return getter(item);
+  else
+    return item[getter];   // Default behaviour, just return a field
+}
 
-  return item[columnDef.field];   // Default behaviour, just return the column field
+// Create a DataTable with budget data
+function createBudgetGrid(containerName, data, userColumns, extraGridOptions) {
+  // Add some default settings to the column definitions given by the user
+  var columns = [];
+  $.each(userColumns, function(i, column) {
+    var defaultColumnDefinition = { sType: 'tree', orderDataType: 'tree' };
+    columns[i] = $.extend(defaultColumnDefinition, column);
+  });
+
+  // Custom filtering function. Decides whether an item should be displayed,
+  // based on whether its ancestors are expanded.
+  $.fn.dataTable.ext.search = [
+    function(settings, row, dataIndex) {
+      item = data[dataIndex];
+
+      // Check the parents' status
+      var parent = item.parent;
+      while (parent != null) {
+        if (!parent._expanded)  return false;
+        parent = parent.parent;
+      }
+
+      // Check whether there's any data in this row
+      for (var i in columns) {
+        // The 'i>0' means ignore the description column.
+        if ( i>0 && row[i] != undefined && row[i] != null && row[i] != 0 ) return true;
+      }
+      return false;
+    }
+  ];
+
+  // We need to create a custom sorter to respect the item hierarchy
+  $.fn.dataTable.ext.order['tree'] = function(settings, column) {
+    function simpleComparer(a, b) {
+      var valueGetter = columns[column].data;
+      var x = columnValueExtractor(a, valueGetter) || 0; // For our purposes, not found => 0
+      var y = columnValueExtractor(b, valueGetter) || 0;
+
+      // Google Chrome doesn't do stable sorting [1], i.e. the order of equal elements is
+      // not deterministic. This breaks the multi-level sorting in the edge case of child
+      // nodes belonging to equal parents, which happens for example when there's no data
+      // for certain items. So to break the tie we just use the item id, so we get
+      // deterministic behaviour and children sticking to their parent nodes.
+      // [1]: https://code.google.com/p/v8/issues/detail?id=90
+      return (x == y ? (a.id < b.id ? -1 : 1) : (x < y ? -1 : 1));
+    }
+
+    // A comparison function that can sort a table while keeping the parent-child hierarchy
+    function multiLevelComparer(a, b, ascending) {
+      // Handle trivial cases first...
+      if ( a.parent == b ) {                // Parent-child: don't sort
+        return 1;
+      } else if ( a == b.parent ) {         // Parent-child: don't sort
+        return -1;
+      } else if ( a.parent == b.parent ) {  // Siblings
+        return ascending ? simpleComparer(a, b) : simpleComparer(b, a);
+      }
+
+      // Travel up from the deeper item until the indent is the same, and
+      // we'll eventually end up in one of the trivial cases above
+      if ( a.indent > b.indent ) {
+        return multiLevelComparer(a.parent, b, ascending);
+      } else if ( a.indent < b.indent ) {
+        return multiLevelComparer(a, b.parent, ascending);
+      } else {
+        return multiLevelComparer(a.parent, b.parent, ascending);
+      }
+    }
+
+    jQuery.fn.dataTableExt.oSort['tree-asc']  = function(a, b) {
+      return multiLevelComparer(a, b, true);
+    };
+
+    jQuery.fn.dataTableExt.oSort['tree-desc'] = function(a, b) {
+      return multiLevelComparer(a, b, false);
+    };
+
+    return data;
+  };
+
+  // Create the data table
+  var grid = $(containerName).DataTable({
+    data: data,
+    paging: false,
+    dom: 't', // Only the table
+    // responsive: true, See https://datatables.net/extensions/responsive/
+    columns: columns,
+    // Sort at startup by the previously chosen column; or by the rightmost column at startup
+    // TODO: Actually, we're going to sort by the second column, I should use the rightmost... with data
+    order: [[1, 'desc']]
+  });
+
+  // Handle toggling of items
+  $(containerName+' tbody').on('click', 'span.toggle', function () {
+    var cell = grid.cell( $(this).parent() )[0][0];
+    var item = data[cell.row];
+    item._expanded = !item._expanded;
+    grid.draw();
+  });
+
+  return grid;
 }
 
 // Create a SlickGrid with budget data
-function createBudgetGrid(containerName, data, userColumns, extraGridOptions) {
+// FIXME: I should get rid of this when we remove SlickGrid
+function createSlickBudgetGrid(containerName, data, userColumns, extraGridOptions) {
 
   // Get a full SlickGrid column definition combining basic defaults with given options
   function getColumnDefinition(options) {
@@ -161,7 +269,7 @@ function createBudgetGrid(containerName, data, userColumns, extraGridOptions) {
       var x = columnValueExtractor(a, args.sortCol) || 0; // For our purposes, not found => 0
       var y = columnValueExtractor(b, args.sortCol) || 0;
 
-      // Google Chrome doesn't do stable sorting [1], i.e. the order of equal elements is 
+      // Google Chrome doesn't do stable sorting [1], i.e. the order of equal elements is
       // not deterministic. This breaks the multi-level sorting in the edge case of child
       // nodes belonging to equal parents, which happens for example when there's no data
       // for certain items. So to break the tie we just use the item id, so we get
