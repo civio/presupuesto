7bc0d3fa8c38d261d548d0b8401ea83f2c63dbf5
David Cabo <david.cabo@gmail.com>
2016-03-07 03:33:27 +0100

    Add programme field to payments loader, as part of civio/presupuesto-management#161.
    Also, while we are at it, relax the null constraints in the table and in the loader, no need to fill the db with empty strings.

--- budget_app/models/payment.py
+++ budget_app/models/payment.py
@@ -44,13 +44,13 @@ class PaymentManager(models.Manager):
 
 class Payment(models.Model):
     budget = models.ForeignKey('Budget')
-    area = models.CharField(max_length=100)
-    programme = models.CharField(max_length=100, default='')
+    area = models.CharField(max_length=100, null=True)
+    programme = models.CharField(max_length=100, null=True)
     functional_category = models.ForeignKey('FunctionalCategory', db_column='functional_category_id', null=True)
     economic_category = models.ForeignKey('EconomicCategory', db_column='economic_category_id', null=True)
     economic_concept = models.CharField(max_length=10, null=True)
     date = models.DateField(null=True)
-    contract_type = models.CharField(max_length=50)
+    contract_type = models.CharField(max_length=50, null=True)
     payee = models.CharField(max_length=200)
     description = models.CharField(max_length=300)
     expense = models.BooleanField()
