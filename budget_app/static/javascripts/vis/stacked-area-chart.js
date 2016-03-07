51e53c5387a838288d2865fec6b63e8ac57dc72f
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2016-03-07 16:46:22 +0100

    Set chart legend at the bottom of the chart

--- budget_app/static/javascripts/vis/stacked-area-chart.js
+++ budget_app/static/javascripts/vis/stacked-area-chart.js
@@ -51,10 +51,6 @@ function StackedAreaChart() {
       $(selector+' .stacked-area-chart-legend').remove();
     }
 
-    // Add legend
-    _this.legend = d3.select(selector).append('div')
-      .attr('class', 'stacked-area-chart-legend');
-
     // Setup Popover
     _this.$popover = $(selector).find('.popover');
 
@@ -63,6 +59,10 @@ function StackedAreaChart() {
         .attr('class', 'stacked-area-chart')
       .append('g')
         .attr('transform', 'translate(' + _this.margin.left + ',' + _this.margin.top + ')');
+
+    // Add legend
+    _this.legend = d3.select(selector).append('div')
+      .attr('class', 'stacked-area-chart-legend');
 
     // Set dimensions & add resize event
     setDimensions();
