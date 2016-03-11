9b638e4bee9e234d349ee679d7498eca1c8a94d2
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2016-03-11 16:42:39 +0100

    Add count-css-rules script based on settings.DEBUG value

--- project/settings.py
+++ project/settings.py
@@ -264,7 +264,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "budget_app.context_processors.show_options_processor",
     "budget_app.context_processors.main_entity_processor",
     "budget_app.context_processors.data_sources_processor",
-    "budget_app.context_processors.search_entities_processor"
+    "budget_app.context_processors.search_entities_processor",
+    "budget_app.context_processors.debug"
 )
 
 SEARCH_CONFIG = ENV.get('SEARCH_CONFIG', 'pg_catalog.english')
