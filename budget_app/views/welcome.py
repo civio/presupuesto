170a4fd648ffeb6dd348b334a699f4690295797a
David Cabo <david.cabo@gmail.com>
2016-03-15 14:24:05 +0100

    Fix subprogramme list in homepage

--- budget_app/views/welcome.py
+++ budget_app/views/welcome.py
@@ -1,7 +1,7 @@
 from coffin.shortcuts import render_to_response
 from django.conf import settings
 from django.utils.translation import ugettext as _
-from budget_app.models import Budget, Entity, FunctionalCategory, BudgetBreakdown
+from budget_app.models import Budget, Entity, FunctionalCategory, BudgetBreakdown, BudgetItem
 from helpers import *
 from random import sample
 
@@ -12,24 +12,25 @@ def welcome(request):
 
     # Retrieve front page examples
     populate_latest_budget(c)
+    # TODO: Can we get rid of this?
     c['featured_programmes'] = list(FunctionalCategory.objects
                                 .filter(budget=c['latest_budget'])
-                                .filter(programme__in=settings.FEATURED_PROGRAMMES))
+                                .filter(programme__in=settings.FEATURED_PROGRAMMES)
+                                .filter(subprogramme=None))
 
     # Decide whether we're going to show budget or execution figures
-    use_actual = False
-    for programme in c['featured_programmes']:
-        if (BudgetItem.objects
-                    .filter(functional_category=programme)
+    use_actual = (BudgetItem.objects
+                    .filter(budget=c['latest_budget'])
                     .filter(budget__status='')  # Don't use partially executed budgets
-                    .filter(actual=True).count()) > 0:
-            use_actual = True
-            break
+                    .filter(actual=True).count() > 0)
 
     # Calculate subtotals for the selected programmes
     c['breakdown'] = BudgetBreakdown(['programme'])
-    for programme in c['featured_programmes']:
-        for item in programme.budgetitem_set.filter(actual=use_actual):
-            c['breakdown'].add_item(c['latest_budget'].year, item)
+    items = BudgetItem.objects \
+                        .filter(budget=c['latest_budget']) \
+                        .filter(functional_category__programme__in=settings.FEATURED_PROGRAMMES) \
+                        .filter(actual=use_actual)
+    for item in items:
+        c['breakdown'].add_item(c['latest_budget'].year, item)
 
     return render_to_response('welcome/index.html', c)
