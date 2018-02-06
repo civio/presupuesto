3369d71859bc8a22383daab66cc6e48a650947e9
David Cabo <david.cabo@gmail.com>
2018-02-06 17:16:41 +0100

    Extende clean_budgets to remove unused economic categories also civio/presupuesto-management#495

--- budget_app/management/commands/clean_budgets.py
+++ budget_app/management/commands/clean_budgets.py
@@ -13,6 +13,42 @@ class Command(BaseCommand):
     help = u"Borra categorías funcionales no utilizadas de la base de datos, en caso de que sea necesario."
 
     def handle(self, *args, **options):
+        self._clean_unused_functional_categories()
+        self._clean_unused_economic_categories()
+
+    # XXX: This assumes subheadings are not used, i.e. the SimpleBudgetLoader was used. See #495.
+    def _clean_unused_economic_categories(self):
+        # Clean unused headings, i.e. those without related budget items
+        sql = "DELETE " \
+              "FROM economic_categories " \
+              "WHERE economic_categories.id in (" \
+                "select ec.id " \
+                "from " \
+                  "economic_categories ec " \
+                  "left join budget_items bi " \
+                  "on ec.id = bi.economic_category_id " \
+                "where " \
+                  "heading is not null and " \
+                  "bi.id is null" \
+                ")"
+        print "Borrando conceptos no utilizadas..."
+        self._execute_transaction(sql)
+
+        # Clean unused articles, i.e. those without children headings (after those were cleaned)
+        sql = "DELETE " \
+              "FROM economic_categories " \
+              "WHERE " \
+                "heading is null and " \
+                "economic_categories.article not in (" \
+                  "select article " \
+                  "from economic_categories " \
+                  "where heading is not null" \
+                  ")"
+        print "Borrando artículos no utilizadas..."
+        self._execute_transaction(sql)
+
+    # XXX: This may not work correctly when subprogrammes are enabled. See #495
+    def _clean_unused_functional_categories(self):
         sql = "DELETE " \
               "FROM functional_categories " \
               "WHERE functional_categories.id in (" \
@@ -27,7 +63,9 @@ class Command(BaseCommand):
                 ")"
 
         print "Borrando categorías funcionales no utilizadas..."
+        self._execute_transaction(sql)
 
+    def _execute_transaction(self, sql):
         # We apparently need to explicitely call a transaction. Otherwise, Django seems to be
         # waiting for something to happen via 'the normal channels' and our raw query doesn't
         # get commited to the database.
