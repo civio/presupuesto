ea94004f4523d8997bcb8f25ae7f3ef11acda20e
tedezed <tedezed>
2020-08-31 14:18:46 +0200

    d3-bundle.js from image or theme

--- docker/entrypoint.d/50-custom.sh
+++ docker/entrypoint.d/50-custom.sh
@@ -2,7 +2,15 @@
 set -e
 
 mkdir -p /app/static/javascripts/vis/
-cp 
 #ln -s $APPDIR/$THEME/node_modules/d3-shape/src/curve/bundle.js \
 #  /app/static/javascripts/vis/d3-bundle.js
-ln -s $APPDIR/docker/files/d3-bundle.js /app/static/javascripts/vis/d3-bundle.js
+
+if [ -f $APPDIR/$THEME/d3-bundle.js ]; then
+  echo "[INFO] File d3-bundle.js from theme"
+  ln -s $APPDIR/$THEME/d3-bundle.js \
+    /app/static/javascripts/vis/d3-bundle.js
+else
+  echo "[INFO] File d3-bundle.js from default docker image"
+  ln -s $APPDIR/docker/files/d3-bundle.js \
+    /app/static/javascripts/vis/d3-bundle.js
+fi
