f04b53d1f8496d32a36214cbea3c551c6a1dd000
David Cabo <david.cabo@gmail.com>
2022-11-26 03:17:57 +0100

    Load monitoring goals into database civio/presupuesto-management#1216

--- budget_app/loaders/monitoring_loader.py
+++ budget_app/loaders/monitoring_loader.py
@@ -10,4 +10,88 @@ import re
 class MonitoringLoader(BaseLoader):
 
     def load(self, entity, year, path, status):
-        print u"Cargando objetivos e indicadores para entidad '%s' año %s..." % (entity.name, year)
+        # Find the budget the monitoring relates to
+        budget = Budget.objects.filter(entity=entity, year=year)
+        if not budget:
+            raise Exception("Budget (%s/%s) not found" % (entity.name, year))
+        else:
+            budget = budget[0]
+
+        # Delete previous monitoring data for the given budget if it exists
+        Goal.objects.filter(budget=budget).delete()
+
+        # Read the goals data
+        goals = self.parse_items(os.path.join(path, 'indicadores.csv'), self.parse_goal)
+
+        # Store the data in the database
+        if len(goals) > 0:
+            print u"Cargando objetivos e indicadores para entidad '%s' año %s..." % (entity.name, year)
+            self.load_goals(budget, goals)
+
+
+    def parse_items(self, filename, line_parser):
+        items = []
+        if os.path.isfile(filename):
+            print "Leyendo datos de %s..." % filename
+            reader = csv.reader(open(filename, 'rb'), delimiter=self._get_delimiter())
+            for index, line in enumerate(reader):
+                if re.match("^#", line[0]):         # Ignore comments
+                    continue
+
+                if re.match("^ +$", line[0]):       # Ignore empty lines
+                    continue
+
+                # Finally, we have useful data
+                items.append(line_parser(filename, line))
+        else:
+            print "No se encontró el fichero %s" % filename
+
+        return items
+
+
+    # OVERRIDE THIS!
+    # I don't think it's worth offering a base implementation, not at this point at least, since
+    # every input data we get has a different structure, and there's value in keeping the CSV files
+    # as close as possible to the original database, PDF, XLS, whatever.
+    def parse_goal(self, filename, line):
+        return {}
+
+
+    def load_goals(self, budget, goals):
+        for goal in goals:
+            # Ignore null entries
+            if goal == None:
+                continue
+
+            # TODO: check for duplicates!
+
+            # Fetch functional category (required)
+            fc = FunctionalCategory.objects.filter(area=goal['fc_code'][0:1],
+                                                   policy=goal['fc_code'][0:2],
+                                                   function=goal['fc_code'][0:3],
+                                                   programme=goal['fc_code'],
+                                                   budget=budget)
+            if not fc:
+                print u"ALERTA: No se encuentra la categoría funcional '%s' para '%s'." % (goal['fc_code'], goal['description'])
+                continue
+            else:
+                fc = fc[0]
+
+            # Fetch institutional category, if available
+            ic = InstitutionalCategory.objects.filter(institution=goal['ic_code'][0],
+                                                      section=goal['ic_code'][0:2],
+                                                      department=goal['ic_code'],
+                                                      budget=budget)
+            if not ic:
+                print u"ALERTA: No se encuentra la categoría institutional '%s' para '%s'." % (goal['ic_code'], goal['description'])
+                continue
+            else:
+                ic = ic[0]
+
+
+            # Create the main investment record
+            Goal(   functional_category=fc,
+                    institutional_category=ic,
+                    goal_number=goal['goal_number'],
+                    description=goal['description'],
+                    budget=budget).save()
---
+++ budget_app/models/goal.py
@@ -0,0 +1,26 @@
+from django.template.defaultfilters import slugify
+from django.db import models
+from django.conf import settings
+
+
+class GoalsManager(models.Manager):
+    None
+
+
+class Goal(models.Model):
+    budget = models.ForeignKey('Budget')
+    institutional_category = models.ForeignKey('InstitutionalCategory', db_column='institutional_category_id')
+    functional_category = models.ForeignKey('FunctionalCategory', db_column='functional_category_id')
+    goal_number = models.CharField(max_length=2)
+    description = models.CharField(max_length=200)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = GoalsManager()
+
+    class Meta:
+        app_label = "budget_app"
+        db_table = "goals"
+
+    def __unicode__(self):
+        return self.description
