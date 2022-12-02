d38428de8d91e795145f404a4eee00851d42c95c
David Cabo <david.cabo@gmail.com>
2022-12-03 00:59:07 +0100

    Mark unfinished monitoring issues with FIXME civio/presupuesto-management#1216

--- budget_app/models/goal_activity.py
+++ budget_app/models/goal_activity.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class GoalActivitiesManager(models.Manager):
-    # XXX Temporary
+    # FIXME Temporary
     def get_programme_activities(self, entity, programme_id):
         return self \
             .select_related('goal') \
