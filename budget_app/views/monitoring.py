0d12148d24cd7479a1b8d7c3a5b0d50ff1e5d6f9
David Cabo <david.cabo@gmail.com>
2022-12-13 02:57:04 +0100

    Dump budget summary data for monitoring viz civio/presupuesto-management#1216

--- budget_app/views/monitoring.py
+++ budget_app/views/monitoring.py
@@ -1,12 +1,20 @@
 # -*- coding: UTF-8 -*-
 
 from budget_app.views.helpers import *
-from budget_app.models import Goal
+from budget_app.models import GoalIndicator
 
 def monitoring(request, render_callback=None):
     c = get_context(request, css_class='body-monitoring', title='')
     entity = get_main_entity(c)
     set_entity(c, entity)
+
+    # Add monitoring information
+    c['monitoring_policies'] = GoalIndicator.objects.get_budget_indicators_summary_by_policy(entity.id)
+
+    # FIXME: We're dumping the info as CSV here temporarily
+    populate_entity_descriptions(c, entity)
+    for p in c['monitoring_policies']:
+        print('%s,%s,"%s",%s,%s' % (p[0], p[1], c['descriptions']['functional'][p[1]], p[2], p[3]))
 
     # if parameter widget defined use policies/widget template instead of policies/show
     template = 'monitoring/index_widget.html' if isWidget(request) else 'monitoring/index.html'
