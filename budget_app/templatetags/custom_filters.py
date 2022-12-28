f5cf34f1d6d5e0655497ac021113451cf3c209e8
David Cabo <david.cabo@gmail.com>
2022-12-28 20:53:58 +0100

    Format number with Python 2.6-compatible syntx civio/presupuesto-management#1216

--- budget_app/templatetags/custom_filters.py
+++ budget_app/templatetags/custom_filters.py
@@ -31,10 +31,16 @@ def split(s, pattern):
     return s.split(pattern)
 
 @register.filter()
+# After more than an hour fighting with this, I couldn't set the locale to get Python
+# to use the right thousands separator, so I'm doing this. :/
+# Oh, and then found out that Madrid is running under Python 2.6, so the `{,d}`
+# syntax is not supported. So we'll do it ourselves.
+# Adapted from https://www.netnea.com/cms/2013/11/08/thousands-separators-for-numbers-in-python-2-6/
 def add_thousands_separator(value, language):
-    formatted = format(value, ',d')
-    # After more than an hour fighting with this, I couldn't set the locale to get Python
-    # to use the right thousands separator, so I'm doing this. :/
-    if language!='en':
-        formatted = formatted.replace(',', '.')
-    return formatted
+    separator = ',' if language=='en' else '.'
+    s = str(value)
+    groups = []
+    while s and s[-1].isdigit():
+        groups.append(s[-3:])
+        s = s[:-3]
+    return s + separator.join(reversed(groups))
