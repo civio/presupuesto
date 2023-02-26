b2585b68bcbc0844c9c620d26fbe7db57258816d
David Cabo <david.cabo@gmail.com>
2023-02-26 17:16:21 +0100

    Add goal count to monitoring viz civio/presupuesto-management#1213

--- budget_app/views/monitoring.py
+++ budget_app/views/monitoring.py
@@ -30,13 +30,24 @@ def monitoring(request, render_callback=None):
             }
 
         monitoring_data[policy_id]['value_'+year] = progress
-        monitoring_data[policy_id]['objectives_'+year] = 1234   # FIXME
         monitoring_data[policy_id]['total_'+year] = indicators
 
         # Keep track of available years, and the latest year with actual content
         monitoring_years.add(year)
         if progress>0:
             last_monitoring_year = max(last_monitoring_year, year)
+
+    # Expand information with count of goals.
+    # Note that, because some goals don't have indicators, we can't do it in one go,
+    # the previous call would miss them.
+    for count in GoalIndicator.objects.get_monitoring_goals_count_by_policy(entity.id):
+        year = str(count[0])
+        policy_id = count[1]
+        # We shouldn't find a new policy at this point, but we do. The Public Debt
+        # policy in Madrid has (a few) goals, but no indicators at all. We just ignore it,
+        # drawing it wouldn't make much sense: there can never be a progress percentage.
+        if policy_id in monitoring_data:
+            monitoring_data[policy_id]['objectives_'+year] = count[2]
 
     # Output data as JSON to print it in the template's Javascript
     c['monitoring_data'] = json.dumps(sorted(monitoring_data.values()))
