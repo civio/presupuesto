640a226c006cdf53f92256ba2c361a868909ba12
David Cabo <david.cabo@gmail.com>
2023-02-26 14:29:54 +0100

    Use real data for monitoring viz civio/presupuesto-management#1213

--- budget_app/views/monitoring.py
+++ budget_app/views/monitoring.py
@@ -8,15 +8,31 @@ def monitoring(request, render_callback=None):
     entity = get_main_entity(c)
     set_entity(c, entity)
 
-    # Add monitoring information
-    c['monitoring_policies'] = GoalIndicator.objects.get_indicators_summary_by_policy(entity.id)
-
-    # FIXME: We're dumping the info as CSV here temporarily
     populate_entity_descriptions(c, entity)
-    # for p in c['monitoring_policies']:
-    #     print('%s,%s,"%s",%s,%s' % (p[0], p[1], c['descriptions']['functional'][p[1]], p[2], p[3]))
-
     populate_latest_budget(c)
+
+    # Consolidate monitoring information.
+    # We prepare it in the format the visualization expects it, which is arguably a bit
+    # weird. We could adapt the visualization itself to the query results, but this is fine.
+    monitoring_data = {}
+    for summary in GoalIndicator.objects.get_indicators_summary_by_policy(entity.id):
+        year = str(summary[0])
+        policy_id = summary[1]
+        indicators = summary[3]
+        progress = summary[2] / indicators * 100.0
+
+        if not policy_id in monitoring_data:
+            monitoring_data[policy_id] = {
+                'code': policy_id,
+                'label': c['descriptions']['functional'][policy_id]
+            }
+
+        monitoring_data[policy_id]['value_'+year] = progress
+        monitoring_data[policy_id]['objectives_'+year] = 1234   # FIXME
+        monitoring_data[policy_id]['total_'+year] = indicators
+
+    # Output data as JSON to print it in the template's Javascript
+    c['monitoring_data'] = json.dumps(monitoring_data.values())
 
     template = 'monitoring/index_widget.html' if isWidget(request) else 'monitoring/index.html'
 
