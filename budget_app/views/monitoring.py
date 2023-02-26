d7de53e8487082b68f1831d6e9c1c30b13668cb6
David Cabo <david.cabo@gmail.com>
2023-02-26 16:28:15 +0100

    Remove hardcoded year range civio/presupuesto-management#1213

--- budget_app/views/monitoring.py
+++ budget_app/views/monitoring.py
@@ -15,6 +15,8 @@ def monitoring(request, render_callback=None):
     # We prepare it in the format the visualization expects it, which is arguably a bit
     # weird. We could adapt the visualization itself to the query results, but this is fine.
     monitoring_data = {}
+    monitoring_years = set()
+    last_monitoring_year = None
     for summary in GoalIndicator.objects.get_indicators_summary_by_policy(entity.id):
         year = str(summary[0])
         policy_id = summary[1]
@@ -31,8 +33,15 @@ def monitoring(request, render_callback=None):
         monitoring_data[policy_id]['objectives_'+year] = 1234   # FIXME
         monitoring_data[policy_id]['total_'+year] = indicators
 
+        # Keep track of available years, and the latest year with actual content
+        monitoring_years.add(year)
+        if progress>0:
+            last_monitoring_year = max(last_monitoring_year, year)
+
     # Output data as JSON to print it in the template's Javascript
     c['monitoring_data'] = json.dumps(sorted(monitoring_data.values()))
+    c['monitoring_years'] = json.dumps(sorted(monitoring_years))
+    c['last_monitoring_year'] = last_monitoring_year
 
     template = 'monitoring/index_widget.html' if isWidget(request) else 'monitoring/index.html'
 
