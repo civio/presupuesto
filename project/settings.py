d1130228a07b11ae44b627fe4014decd9265326d
David Cabo <david.cabo@gmail.com>
2023-08-24 03:26:42 +0200

    Upgrade Django to 1.7.11
    From the release notes: "Django now has built-in support for schema migrations. It allows models to be updated, changed, and deleted by creating migration files that represent the model changes and which can be run on any development, staging or production database." See https://web.archive.org/web/20160404221407/https://docs.djangoproject.com/en/1.7/topics/migrations/#upgrading-from-south for instructions on upgrade.
    
    Also from the notes: "The syncdb command has been deprecated in favor of the new migrate command. migrate takes the same arguments as syncdb used to plus a few more, so it’s safe to just change the name you’re calling and nothing else."
    
    civio/presupuesto-management#80

--- project/settings.py
+++ project/settings.py
@@ -200,7 +200,6 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'django_jasmine',
-    'south',
     'compressor',
     THEME,
     'budget_app'
