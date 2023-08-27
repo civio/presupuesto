4464e5a3b33fbee0a6f3d4226e776c8024a41a6a
David Cabo <david.cabo@gmail.com>
2023-08-28 00:25:06 +0200

    Refactor URL generation to use URL names instead of dotted paths. I do not like this, and I am not alone (https://code.djangoproject.com/ticket/28029), but it is what it is: Django 1.10 removes the dotted path approach. civio/presupuesto-management#80

--- project/urls.py
+++ project/urls.py
@@ -11,7 +11,7 @@ from django.shortcuts import render
 if len(settings.LANGUAGES) > 1:
     budget_app_urlpatterns = patterns('',
         url(r'^i18n/', include('django.conf.urls.i18n')),
-        url(r'^$', lambda x: HttpResponseRedirect(reverse('budget_app_welcome'))),
+        url(r'^$', lambda x: HttpResponseRedirect(reverse('welcome'))),
     )
     url_patterns = i18n_patterns
 else:
@@ -20,17 +20,17 @@ else:
 
 # Add the application paths
 budget_app_urlpatterns += url_patterns('budget_app.views',
-    url(r'^/?$', 'welcome', name="budget_app_welcome"),
+    url(r'^/?$', 'welcome', name="welcome"),
 
     url(r'^resumen$', 'budgets', name="budgets"),
 
     url(r'^glosario$', 'terms', name="glossary"),
 
-    url(r'^busqueda$', 'search'),
+    url(r'^busqueda$', 'search', name="search"),
 
-    url(r'^recibo$', 'tax_receipt', name="tax-receipt"),
+    url(r'^recibo$', 'tax_receipt', name="tax_receipt"),
 
-    url(r'^visita-guiada$', 'guided_visit', name="guided-visit"),
+    url(r'^visita-guiada$', 'guided_visit', name="guided_visit"),
 
     url(r'^politica-de-cookies$', 'pages', name="pages"),
 
@@ -40,7 +40,7 @@ budget_app_urlpatterns += url_patterns('budget_app.views',
 
     # Investments
     url(r'^inversiones$', 'investments', name="investments"),
-    url(r'^inversiones/(?P<id>[0-9A-Z]+)/(?P<title>.+)$', 'investments_show', name="investments-show"),
+    url(r'^inversiones/(?P<id>[0-9A-Z]+)/(?P<title>.+)$', 'investments_show', name="investments_show"),
 
     # Main investments
     url(r"^inversiones-principales$", "main_investments", name="main_investments"),
@@ -50,54 +50,54 @@ budget_app_urlpatterns += url_patterns('budget_app.views',
 
     # Policies (top)
     url(r'^politicas$', 'policies', name="policies"),
-    url(r'^politicas/(?P<id>[0-9]+)$', 'policies_show', name="policies-show"),
-    url(r'^politicas/(?P<id>[0-9]+)/(?P<title>.+)$', 'policies_show', name="policies-show-2"),
+    url(r'^politicas/(?P<id>[0-9]+)$', 'policies_show', name="policies_show"),
+    url(r'^politicas/(?P<id>[0-9]+)/(?P<title>.+)$', 'policies_show', name="policies_show"),
 
     # Programme pages
-    url(r'^programas$', 'programmes_show', name="policies-programmes"),
-    url(r'^programas/(?P<id>[0-9A-Z]+)$', 'programmes_show', name="policies-programmes-2"),
-    url(r'^programas/(?P<id>[0-9A-Z]+)/(?P<title>.+)$', 'programmes_show', name="policies-programmes-3"),
+    url(r'^programas$', 'programmes_show', name="programmes"),
+    url(r'^programas/(?P<id>[0-9A-Z]+)$', 'programmes_show', name="programmes_show"),
+    url(r'^programas/(?P<id>[0-9A-Z]+)/(?P<title>.+)$', 'programmes_show', name="programmes_show"),
 
     # Subprogramme pages
-    url(r'^subprogramas$', 'subprogrammes_show', name="policies-subprogrammes"),
-    url(r'^subprogramas/(?P<id>[0-9A-Z]+)$', 'subprogrammes_show', name="policies-subprogrammes-2"),
-    url(r'^subprogramas/(?P<id>[0-9A-Z]+)/(?P<title>.+)$', 'subprogrammes_show', name="policies-subprogrammes-3"),
+    url(r'^subprogramas$', 'subprogrammes_show', name="subprogrammes"),
+    url(r'^subprogramas/(?P<id>[0-9A-Z]+)$', 'subprogrammes_show', name="subprogrammes_show"),
+    url(r'^subprogramas/(?P<id>[0-9A-Z]+)/(?P<title>.+)$', 'subprogrammes_show', name="subprogrammes_show"),
 
     # Expense pages (economic breakdown)
-    url(r'^articulos/g$', 'expense_articles_show', name="policies-expense-articles"),
-    url(r'^articulos/g/(?P<id>[0-9]+)$', 'expense_articles_show', name="policies-expense-articles-2"),
-    url(r'^articulos/g/(?P<id>[0-9]+)/(?P<title>.+)$', 'expense_articles_show', name="policies-expense-articles-3"),
+    url(r'^articulos/g$', 'expense_articles_show', name="expense_articles"),
+    url(r'^articulos/g/(?P<id>[0-9]+)$', 'expense_articles_show', name="expense_articles_show"),
+    url(r'^articulos/g/(?P<id>[0-9]+)/(?P<title>.+)$', 'expense_articles_show', name="expense_articles_show"),
 
     # Income pages
-    url(r'^articulos/i$', 'income_articles_show', name="policies-income-articles"),
-    url(r'^articulos/i/(?P<id>[0-9]+)$', 'income_articles_show', name="policies-income-articles-2"),
-    url(r'^articulos/i/(?P<id>[0-9]+)/(?P<title>.+)$', 'income_articles_show', name="policies-income-articles-3"),
+    url(r'^articulos/i$', 'income_articles_show', name="income_articles"),
+    url(r'^articulos/i/(?P<id>[0-9]+)$', 'income_articles_show', name="income_articles_show"),
+    url(r'^articulos/i/(?P<id>[0-9]+)/(?P<title>.+)$', 'income_articles_show', name="income_articles_show"),
 
     # Sections
     url(r'^secciones$', 'sections_show', name="sections"),
-    url(r'^secciones/(?P<id>[0-9A-Z]+)/(?P<title>.+)$', 'sections_show', name="sections-show"),
+    url(r'^secciones/(?P<id>[0-9A-Z]+)/(?P<title>.+)$', 'sections_show', name="sections_show"),
 
     # Child entities
-    url(r'^entidades/(?P<id>[0-9A-Z]+)/politicas$', 'entities_policies', name="entities-policies"),
-    url(r'^entidades/(?P<id>[0-9A-Z]+)/politicas/(?P<policy_id>[0-9]+)/(?P<title>.+)$', 'entities_policies_show', name="entities-policies-show"),
-    url(r'^entidades/(?P<id>[0-9A-Z]+)/programas$', 'entities_programmes', name="entities-programmes"),
-    url(r'^entidades/(?P<id>[0-9A-Z]+)/programas/(?P<programme_id>[0-9A-Z]+)/(?P<title>.+)$', 'entities_programmes_show', name="entities-programmes-show"),
-    url(r'^entidades/(?P<id>[0-9A-Z]+)/articulos/i$', 'entities_income_articles_show', name="entities-income-articles"),
-    url(r'^entidades/(?P<id>[0-9A-Z]+)/articulos/i/(?P<article_id>[0-9]+)/(?P<title>.+)$', 'entities_income_articles_show', name="entities-income-articles-show"),
-    url(r'^entidades/(?P<id>[0-9A-Z]+)/articulos/g$', 'entities_expense_articles_show', name="entities-expense-articles"),
-    url(r'^entidades/(?P<id>[0-9A-Z]+)/articulos/g/(?P<article_id>[0-9]+)/(?P<title>.+)$', 'entities_expense_articles_show', name="entities-expense-articles-show"),
-    url(r'^entidades/(?P<id>[0-9A-Z]+)/pagos$', 'entities_payments', name="entities-payments"),
+    url(r'^entidades/(?P<id>[0-9A-Z]+)/politicas$', 'entities_policies', name="entities_policies"),
+    url(r'^entidades/(?P<id>[0-9A-Z]+)/politicas/(?P<policy_id>[0-9]+)/(?P<title>.+)$', 'entities_policies_show', name="entities_policies_show"),
+    url(r'^entidades/(?P<id>[0-9A-Z]+)/programas$', 'entities_programmes', name="entities_programmes"),
+    url(r'^entidades/(?P<id>[0-9A-Z]+)/programas/(?P<programme_id>[0-9A-Z]+)/(?P<title>.+)$', 'entities_programmes_show', name="entities_programmes_show"),
+    url(r'^entidades/(?P<id>[0-9A-Z]+)/articulos/i$', 'entities_income_articles_show', name="entities_income_articles"),
+    url(r'^entidades/(?P<id>[0-9A-Z]+)/articulos/i/(?P<article_id>[0-9]+)/(?P<title>.+)$', 'entities_income_articles_show', name="entities_income_articles_show"),
+    url(r'^entidades/(?P<id>[0-9A-Z]+)/articulos/g$', 'entities_expense_articles_show', name="entities_expense_articles"),
+    url(r'^entidades/(?P<id>[0-9A-Z]+)/articulos/g/(?P<article_id>[0-9]+)/(?P<title>.+)$', 'entities_expense_articles_show', name="entities_expense_articles_show"),
+    url(r'^entidades/(?P<id>[0-9A-Z]+)/pagos$', 'entities_payments', name="entities_payments"),
     url(r'^entidades/(?P<id>[0-9A-Z]+)/pagos/search$', 'entities_payments_search', name="entities_payments_search"),
 
     # Counties
-    url(r'^comarcas$', 'counties'),
+    url(r'^comarcas$', 'counties', name='counties'),
     url(r'^comarcas/(?P<county_slug>[a-z\-]+)$', 'counties_show'),
     url(r'^comarcas/(?P<county_slug>[a-z\-]+)/ingresos/(?P<id>[0-9]+)$', 'counties_show_income'),
     url(r'^comarcas/(?P<county_slug>[a-z\-]+)/gastosf/(?P<id>[0-9]+)$', 'counties_show_functional'),
     url(r'^comarcas/(?P<county_slug>[a-z\-]+)/gastos/(?P<id>[0-9]+)$', 'counties_show_expense'),
 
     # Towns
-    url(r'^municipios$', 'towns'),
+    url(r'^municipios$', 'towns', name='towns'),
     url(r'^municipios/(?P<town_slug>[a-z\-]+)$', 'towns_show'),
     url(r'^municipios/(?P<town_slug>[a-z\-]+)/ingresos/(?P<id>[0-9]+)$', 'towns_show_income'),
     url(r'^municipios/(?P<town_slug>[a-z\-]+)/gastosf/(?P<id>[0-9]+)$', 'towns_show_functional'),
@@ -113,62 +113,62 @@ budget_app_urlpatterns += url_patterns('budget_app.views',
     #
 
     # Policies
-    url(r'^politicas/(?P<id>[0-9]+)_functional\.(?P<format>.+)$', 'functional_policy_breakdown'),
-    url(r'^politicas/(?P<id>[0-9]+)_economic\.(?P<format>.+)$', 'economic_policy_breakdown'),
-    url(r'^politicas/(?P<id>[0-9]+)_funding\.(?P<format>.+)$', 'funding_policy_breakdown'),
-    url(r'^politicas/(?P<id>[0-9]+)_institutional\.(?P<format>.+)$', 'institutional_policy_breakdown'),
+    url(r'^politicas/(?P<id>[0-9]+)_functional\.(?P<format>.+)$', 'functional_policy_breakdown', name='functional_policy_breakdown'),
+    url(r'^politicas/(?P<id>[0-9]+)_economic\.(?P<format>.+)$', 'economic_policy_breakdown', name='economic_policy_breakdown'),
+    url(r'^politicas/(?P<id>[0-9]+)_funding\.(?P<format>.+)$', 'funding_policy_breakdown', name='funding_policy_breakdown'),
+    url(r'^politicas/(?P<id>[0-9]+)_institutional\.(?P<format>.+)$', 'institutional_policy_breakdown', name='institutional_policy_breakdown'),
 
     # Programmes
-    url(r'^programas/(?P<id>[0-9A-Z]+)_functional\.(?P<format>.+)$', 'functional_programme_breakdown'),
-    url(r'^programas/(?P<id>[0-9A-Z]+)_economic\.(?P<format>.+)$', 'economic_programme_breakdown'),
-    url(r'^programas/(?P<id>[0-9A-Z]+)_funding\.(?P<format>.+)$', 'funding_programme_breakdown'),
-    url(r'^programas/(?P<id>[0-9A-Z]+)_institutional\.(?P<format>.+)$', 'institutional_programme_breakdown'),
+    url(r'^programas/(?P<id>[0-9A-Z]+)_functional\.(?P<format>.+)$', 'functional_programme_breakdown', name='functional_programme_breakdown'),
+    url(r'^programas/(?P<id>[0-9A-Z]+)_economic\.(?P<format>.+)$', 'economic_programme_breakdown', name='economic_programme_breakdown'),
+    url(r'^programas/(?P<id>[0-9A-Z]+)_funding\.(?P<format>.+)$', 'funding_programme_breakdown', name='funding_programme_breakdown'),
+    url(r'^programas/(?P<id>[0-9A-Z]+)_institutional\.(?P<format>.+)$', 'institutional_programme_breakdown', name='institutional_programme_breakdown'),
 
-    # Programmes
-    url(r'^subprogramas/(?P<id>[0-9A-Z]+)_economic\.(?P<format>.+)$', 'economic_subprogramme_breakdown'),
-    url(r'^subprogramas/(?P<id>[0-9A-Z]+)_funding\.(?P<format>.+)$', 'funding_subprogramme_breakdown'),
-    url(r'^subprogramas/(?P<id>[0-9A-Z]+)_institutional\.(?P<format>.+)$', 'institutional_subprogramme_breakdown'),
+    # Subprogrammes
+    url(r'^subprogramas/(?P<id>[0-9A-Z]+)_economic\.(?P<format>.+)$', 'economic_subprogramme_breakdown', name='economic_subprogramme_breakdown'),
+    url(r'^subprogramas/(?P<id>[0-9A-Z]+)_funding\.(?P<format>.+)$', 'funding_subprogramme_breakdown', name='funding_subprogramme_breakdown'),
+    url(r'^subprogramas/(?P<id>[0-9A-Z]+)_institutional\.(?P<format>.+)$', 'institutional_subprogramme_breakdown', name='institutional_subprogramme_breakdown'),
 
     # Articles
-    url(r'^articulos/(?P<id>[0-9]+)_functional\.(?P<format>.+)$', 'functional_article_expenditures_breakdown'),
-    url(r'^articulos/(?P<id>[0-9]+)_economic_revenues\.(?P<format>.+)$', 'economic_article_revenues_breakdown'),
-    url(r'^articulos/(?P<id>[0-9]+)_economic_expenditures\.(?P<format>.+)$', 'economic_article_expenditures_breakdown'),
-    url(r'^articulos/(?P<id>[0-9]+)_funding_revenues\.(?P<format>.+)$', 'funding_article_revenues_breakdown'),
-    url(r'^articulos/(?P<id>[0-9]+)_funding_expenditures\.(?P<format>.+)$', 'funding_article_expenditures_breakdown'),
-    url(r'^articulos/(?P<id>[0-9]+)_institutional_revenues\.(?P<format>.+)$', 'institutional_article_revenues_breakdown'),
-    url(r'^articulos/(?P<id>[0-9]+)_institutional_expenditures\.(?P<format>.+)$', 'institutional_article_expenditures_breakdown'),
+    url(r'^articulos/(?P<id>[0-9]+)_functional\.(?P<format>.+)$', 'functional_article_expenditures_breakdown', name='functional_article_expenditures_breakdown'),
+    url(r'^articulos/(?P<id>[0-9]+)_economic_revenues\.(?P<format>.+)$', 'economic_article_revenues_breakdown', name='economic_article_revenues_breakdown'),
+    url(r'^articulos/(?P<id>[0-9]+)_economic_expenditures\.(?P<format>.+)$', 'economic_article_expenditures_breakdown', name='economic_article_expenditures_breakdown'),
+    url(r'^articulos/(?P<id>[0-9]+)_funding_revenues\.(?P<format>.+)$', 'funding_article_revenues_breakdown', name='funding_article_revenues_breakdown'),
+    url(r'^articulos/(?P<id>[0-9]+)_funding_expenditures\.(?P<format>.+)$', 'funding_article_expenditures_breakdown', name='funding_article_expenditures_breakdown'),
+    url(r'^articulos/(?P<id>[0-9]+)_institutional_revenues\.(?P<format>.+)$', 'institutional_article_revenues_breakdown', name='institutional_article_revenues_breakdown'),
+    url(r'^articulos/(?P<id>[0-9]+)_institutional_expenditures\.(?P<format>.+)$', 'institutional_article_expenditures_breakdown', name='institutional_article_expenditures_breakdown'),
 
     # Sections
-    url(r'^secciones/(?P<id>[0-9A-Z]+)_functional\.(?P<format>.+)$', 'functional_section_breakdown'),
-    url(r'^secciones/(?P<id>[0-9A-Z]+)_economic\.(?P<format>.+)$', 'economic_section_breakdown'),
+    url(r'^secciones/(?P<id>[0-9A-Z]+)_functional\.(?P<format>.+)$', 'functional_section_breakdown', name='functional_section_breakdown'),
+    url(r'^secciones/(?P<id>[0-9A-Z]+)_economic\.(?P<format>.+)$', 'economic_section_breakdown', name='economic_section_breakdown'),
 
     # Investments
-    url(r'^inversiones/(?P<slug>.+)_inversiones\.(?P<format>.+)$', 'entity_investments_breakdown'),
-    url(r'^inversiones/(?P<slug>.+)_inversiones_(?P<id>[0-9A-Z]+)\.(?P<format>.+)$', 'entity_investment_line_breakdown'),
+    url(r'^inversiones/(?P<slug>.+)_inversiones\.(?P<format>.+)$', 'entity_investments_breakdown', name='entity_investments_breakdown'),
+    url(r'^inversiones/(?P<slug>.+)_inversiones_(?P<id>[0-9A-Z]+)\.(?P<format>.+)$', 'entity_investment_line_breakdown', name='entity_investment_line_breakdown'),
 
     # Main investments
-    url(r'^inversiones-principales/(?P<slug>.+)_inversiones\.(?P<format>.+)$', 'entity_main_investments_breakdown'),
+    url(r'^inversiones-principales/(?P<slug>.+)_inversiones\.(?P<format>.+)$', 'entity_main_investments_breakdown', name='entity_main_investments_breakdown'),
 
     # Monitoring
-    url(r'^politicas/(?P<id>.+)_objetivos\.(?P<format>.+)$', 'policy_monitoring_breakdown'),
-    url(r'^programas/(?P<id>.+)_objetivos\.(?P<format>.+)$', 'programme_monitoring_breakdown'),
+    url(r'^politicas/(?P<id>.+)_objetivos\.(?P<format>.+)$', 'policy_monitoring_breakdown', name='policy_monitoring_breakdown'),
+    url(r'^programas/(?P<id>.+)_objetivos\.(?P<format>.+)$', 'programme_monitoring_breakdown', name='programme_monitoring_breakdown'),
 
     # Entities lists
-    url(r'^gastos_entidades_(?P<level>.+)\.(?P<format>.+)$', 'entities_expenses'),
-    url(r'^ingresos_entidades_(?P<level>.+)\.(?P<format>.+)$', 'entities_income'),
+    url(r'^gastos_entidades_(?P<level>.+)\.(?P<format>.+)$', 'entities_expenses', name='entities_expenses'),
+    url(r'^ingresos_entidades_(?P<level>.+)\.(?P<format>.+)$', 'entities_income', name='entities_income'),
 
     # Entity income/expenses
-    url(r'^(?P<level>.+)_(?P<slug>.+)_gastos\.(?P<format>.+)$', 'entity_expenses'),
-    url(r'^(?P<level>.+)_(?P<slug>.+)_gastosf\.(?P<format>.+)$', 'entity_functional'),
-    url(r'^(?P<level>.+)_(?P<slug>.+)_gastosi\.(?P<format>.+)$', 'entity_institutional'),
-    url(r'^(?P<level>.+)_(?P<slug>.+)_ingresos\.(?P<format>.+)$', 'entity_income'),
+    url(r'^(?P<level>.+)_(?P<slug>.+)_gastos\.(?P<format>.+)$', 'entity_expenses', name='entity_expenses'),
+    url(r'^(?P<level>.+)_(?P<slug>.+)_gastosf\.(?P<format>.+)$', 'entity_functional', name='entity_functional'),
+    url(r'^(?P<level>.+)_(?P<slug>.+)_gastosi\.(?P<format>.+)$', 'entity_institutional', name='entity_institutional'),
+    url(r'^(?P<level>.+)_(?P<slug>.+)_ingresos\.(?P<format>.+)$', 'entity_income', name='entity_income'),
 
-    url(r'^(?P<level>.+)_(?P<slug>.+)_gastosf_(?P<id>[0-9]+)\.(?P<format>.+)$', 'entity_article_functional'),
-    url(r'^(?P<level>.+)_(?P<slug>.+)_gastos_(?P<id>[0-9]+)\.(?P<format>.+)$', 'entity_article_expenses'),
-    url(r'^(?P<level>.+)_(?P<slug>.+)_ingresos_(?P<id>[0-9]+)\.(?P<format>.+)$', 'entity_article_income'),
+    url(r'^(?P<level>.+)_(?P<slug>.+)_gastosf_(?P<id>[0-9]+)\.(?P<format>.+)$', 'entity_article_functional', name='entity_article_functional'),
+    url(r'^(?P<level>.+)_(?P<slug>.+)_gastos_(?P<id>[0-9]+)\.(?P<format>.+)$', 'entity_article_expenses', name='entity_article_expenses'),
+    url(r'^(?P<level>.+)_(?P<slug>.+)_ingresos_(?P<id>[0-9]+)\.(?P<format>.+)$', 'entity_article_income', name='entity_article_income'),
 
     # Payments
-    url(r'^(?P<slug>.+)_pagos\.(?P<format>.+)$', 'entity_payments'),
+    url(r'^(?P<slug>.+)_pagos\.(?P<format>.+)$', 'entity_payments', name='entity_payments'),
 )
 
 # Add extra application paths, not i18n
