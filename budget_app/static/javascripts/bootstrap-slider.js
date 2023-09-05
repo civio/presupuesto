cd5535093c43b7800ea47f54abef7f57f2aca319
carmen-tm <carmentorremolina@gmail.com>
2023-09-05 18:38:00 +0200

    a11y: Add accesible aria-label name to elements with slider role for civio/presupuesto-management#1278

--- budget_app/static/javascripts/bootstrap-slider.js
+++ budget_app/static/javascripts/bootstrap-slider.js
@@ -404,12 +404,14 @@ function _typeof(obj) { return obj && typeof Symbol !== "undefined" && obj.const
 				sliderMinHandle = document.createElement("div");
 				sliderMinHandle.className = "slider-handle min-slider-handle";
 				sliderMinHandle.setAttribute('role', 'slider');
+				sliderMaxHandle.setAttribute('aria-label', 'Valor mínimo del rango de fechas');
 				sliderMinHandle.setAttribute('aria-valuemin', this.options.min);
 				sliderMinHandle.setAttribute('aria-valuemax', this.options.max);
 
 				sliderMaxHandle = document.createElement("div");
 				sliderMaxHandle.className = "slider-handle max-slider-handle";
 				sliderMaxHandle.setAttribute('role', 'slider');
+				sliderMaxHandle.setAttribute('aria-label', 'Valor máximo del rango de fechas');
 				sliderMaxHandle.setAttribute('aria-valuemin', this.options.min);
 				sliderMaxHandle.setAttribute('aria-valuemax', this.options.max);
 
