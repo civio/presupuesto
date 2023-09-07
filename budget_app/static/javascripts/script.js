7109994ac91c1a9198fc11a22b5c15fbee769e0e
David Cabo <david.cabo@gmail.com>
2023-09-07 17:02:23 +0200

    Remove old workaround around iPhone bug, not needed anymore I think. This seems to be creating accessibility issues in civio/presupuesto-management#1278

--- budget_app/static/javascripts/script.js
+++ budget_app/static/javascripts/script.js
@@ -33,28 +33,4 @@ $(document).ready(function(){
 	if ( $.cookie('embedded')==='1' ) {
 		$('.hide-when-embedded').hide();
 	}
-
-
-	// iPhone Safari Viewport Scaling Bug
-	// See https://webdesignerwall.com/tutorials/iphone-safari-viewport-scaling-bug
-	(function(doc) {
-		var addEvent = 'addEventListener',
-				type = 'gesturestart',
-				qsa = 'querySelectorAll',
-				scales = [1, 1],
-				meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [];
-
-		function fix() {
-			meta.content = 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1];
-			doc.removeEventListener(type, fix, true);
-		}
-
-		if ((meta = meta[meta.length - 1]) && addEvent in doc) {
-			fix();
-			scales = [.25, 1.6];
-			doc[addEvent](type, fix, true);
-		}
-
-	}(document));
-
 });
