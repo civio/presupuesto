85fea8c90df94cbd0349a2353cfc5313ffb05274
David Cabo <david.cabo@gmail.com>
2025-03-19 00:01:06 +0100

    Use Python 3 syntax for prints and imports civio/presupuesto-management#1286

--- budget_app/loaders/glossary_loader.py
+++ budget_app/loaders/glossary_loader.py
@@ -10,13 +10,13 @@ import re
 class GlossaryLoader(object):
     def load(self, filename, language):
 
-        print "Cargando glosario de %s..." % filename
+        print("Cargando glosario de %s..." % filename)
         reader = csv.reader(open(filename, 'rb'))
         for index, line in enumerate(reader):
             if re.match("^#", line[0]):  # Ignore comments
                 continue
 
-            print "  Cargando término %s..." % line[0]
+            print("  Cargando término %s..." % line[0])
             try:
                 term = GlossaryTerm.objects.get(title=line[0], language=language)
                 term.description = line[1]
