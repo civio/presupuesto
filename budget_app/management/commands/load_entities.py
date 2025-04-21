3f104a19e28a597d57d8ad5e1a6958ea948c63ef
David Cabo <david.cabo@gmail.com>
2025-04-22 00:03:08 +0200

    Remove code for obsolete override functionality, never used

--- budget_app/management/commands/load_entities.py
+++ budget_app/management/commands/load_entities.py
@@ -13,11 +13,5 @@ class Command(BaseCommand):
     help = u"Carga la lista de entidades públicas, _reemplazando el actual_"
 
     def handle(self, *args, **options):
-        # Allow overriding the data path from command line
-        # XXX: overriding was useful when data was shared, not inside the themes. Could be removed.
-        if len(args) < 1:
-          path = os.path.join(settings.ROOT_PATH, settings.THEME, 'data') # Default: theme data folder
-        else:
-          path = args[0]
-
+        path = os.path.join(settings.ROOT_PATH, settings.THEME, 'data')
         EntityLoader().load(os.path.join(path, 'entidades.csv'))
